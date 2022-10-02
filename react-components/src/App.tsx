64e5fb38841794888bef7e2dca41e847d0e1363c
Maksym Kantor <max.kantor72@gmail.com>
2022-10-03 01:44:25 +0300

    feat: add 404 page

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './globals.css';
 import Home from './pages/Home';
 import About from 'pages/About';
+import PageNotFound from 'pages/PageNotFound';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
