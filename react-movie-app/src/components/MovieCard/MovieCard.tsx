c3a88ccf94e69cfeb997ee81ce23aeaa8bce5d1f
Maksym Kantor <max.kantor72@gmail.com>
2022-10-13 00:33:23 +0300

    feat: change MovieCard design

--- react-movie-app/src/components/MovieCard/MovieCard.tsx
+++ react-movie-app/src/components/MovieCard/MovieCard.tsx
@@ -1,40 +1,54 @@
+import MovieInfo from 'components/MovieInfo';
 import React from 'react';
 import { IMovie } from 'types';
+import Modal from 'ui/Modal';
 import './MovieCard.css';
 
-interface MovieCardProps {
+interface IMovieCardProps {
   movie: IMovie;
 }
+interface IMovieCardState {
+  isModal: boolean;
+}
 
-export default class MovieCard extends React.Component<MovieCardProps> {
+export default class MovieCard extends React.Component<IMovieCardProps, IMovieCardState> {
+  constructor(props: IMovieCardProps) {
+    super(props);
+    this.state = {
+      isModal: false,
+    };
+  }
   render() {
     return (
-      <div className="movie-card" data-testid="movie-card">
-        <div className="movie-card__poster">
-          <img
-            src={this.props.movie.backdrop_path}
-            alt={`${this.props.movie.title} backdrop image`}
-            className="bg-img"
-          />
-        </div>
-        <div className="movie-card__info">
+      <>
+        <div
+          className="movie-card"
+          onClick={() => this.setState({ isModal: true })}
+          data-testid="movie-card"
+        >
+          <div className="movie-card__poster">
+            <img
+              src={this.props.movie.backdrop_path}
+              alt={`${this.props.movie.title} backdrop image`}
+              className="bg-img"
+            />
+          </div>
           <h3 className="movie-card__title">
             <span className="movie-card__name">{this.props.movie.title} </span>
             <span className="movie-card__release-date">
               ({this.props.movie.release_date.slice(0, 4)})
             </span>
           </h3>
-          <p className="movie-card__genres">{this.props.movie.genres.join(' ')}</p>
-          <p className="movie-card__overview">{this.props.movie.overview}</p>
-          <p className="movie-card__runtime">{this.props.movie.runtime} min.</p>
-          {this.props.movie.vote_average && (
-            <div className="movie-card__rating badge">
-              {this.props.movie.vote_average.toFixed(1)}
-            </div>
-          )}
-          <div className="movie-card__language badge">{this.props.movie.original_language}</div>
+          <div className="movie-card__more-container">
+            <p className="movie-card__more-text">Click to see more</p>
+          </div>
         </div>
-      </div>
+        {this.state.isModal && (
+          <Modal closeCb={() => this.setState({ isModal: false })}>
+            <MovieInfo movie={this.props.movie} />
+          </Modal>
+        )}
+      </>
     );
   }
 }
---
+++ react-movie-app/src/components/MovieInfo/MovieInfo.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { IMovie } from 'types';
+import './MovieInfo.css';
+
+interface IMovieInfoProps {
+  movie: IMovie;
+}
+export default class MovieInfo extends React.Component<IMovieInfoProps> {
+  render() {
+    return (
+      <div className="movie-info">
+        <h3 className="movie-info__title">{this.props.movie.title}</h3>
+        <p className="movie-info__overview">{this.props.movie.overview}</p>
+        <div className="movie-info__grid">
+          <div className="movie-info__column">
+            <p className="movie-info__date">
+              <h4 className="movie-info__subtitle">Release date: </h4>
+              {this.props.movie.release_date}
+            </p>
+            <p className="movie-info__genres">
+              <h4 className="movie-info__subtitle">Genres: </h4>
+              {this.props.movie.genres.join(', ')}
+            </p>
+          </div>
+          <div className="movie-info__column">
+            <p className="movie-info__language">
+              <h4 className="movie-info__subtitle">Original language: </h4>
+              {this.props.movie.original_language}
+            </p>
+            <p className="movie-info__rating">
+              <h4 className="movie-info__subtitle">Rating: </h4>
+              {this.props.movie.vote_average ? this.props.movie.vote_average.toFixed(1) : 'TBA'}
+            </p>
+          </div>
+        </div>
+        <p className="movie-info__runtime">{this.props.movie.runtime} min.</p>
+      </div>
+    );
+  }
+}
