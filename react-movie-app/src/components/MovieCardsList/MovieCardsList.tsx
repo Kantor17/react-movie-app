e713b2c35bb7b9f066edab644eed45217cb1e7f7
Maksym Kantor <max.kantor72@gmail.com>
2022-10-16 18:35:22 +0300

    feat: add message when no movies was found

--- react-movie-app/src/components/MovieCardsList/MovieCardsList.tsx
+++ react-movie-app/src/components/MovieCardsList/MovieCardsList.tsx
@@ -10,9 +10,11 @@ export default class MovieCardsList extends React.Component<IMovieCardsListProps
   render() {
     return (
       <div className="movie-cards-list">
-        {this.props.movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        {this.props.movies.length > 0 ? (
+          this.props.movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+        ) : (
+          <p className="no-movies-msg">Sorry, no movies found.</p>
+        )}
       </div>
     );
   }
