f23ec50bc0a67936c07f9cb50aceb8e39e332133
Maksym Kantor <max.kantor72@gmail.com>
2022-10-31 15:49:54 +0200

    refactor: rewrite Ideas page with hooks(react-hook-form) and func components

--- react-movie-app/src/pages/Ideas/Ideas.tsx
+++ react-movie-app/src/pages/Ideas/Ideas.tsx
@@ -1,42 +1,26 @@
 import Header from 'components/Header';
 import MovieCardsList from 'components/MovieCardsList';
 import MovieForm from 'components/MovieForm';
-import React from 'react';
+import React, { useState } from 'react';
 import { IMovie } from 'types';
 import './Ideas.css';
 
-interface IIdeasState {
-  movies: IMovie[];
-}
-export default class Ideas extends React.Component<Record<string, never>, IIdeasState> {
-  constructor(props: Record<string, never>) {
-    super(props);
-    this.state = {
-      movies: [],
-    };
-  }
+export default function Ideas() {
+  const [movies, setMovies] = useState<IMovie[]>([]);
 
-  addNewMovie = (movie: IMovie) => {
-    this.setState({
-      movies: [...this.state.movies, movie],
-    });
-  };
-
-  render() {
-    return (
-      <div className="ideas">
-        <Header />
-        <main className="main">
-          <div className="container">
-            <h1 className="ideas__heading h1">My ideas</h1>
-            <p className="ideas__explanation">
-              You can create your own movie ideas and see them as cards below the creation form
-            </p>
-            <MovieForm addNewItemCb={this.addNewMovie} />
-            {this.state.movies.length > 0 && <MovieCardsList movies={this.state.movies} />}
-          </div>
-        </main>
-      </div>
-    );
-  }
+  return (
+    <div className="ideas">
+      <Header />
+      <main className="main">
+        <div className="container">
+          <h1 className="ideas__heading h1">My ideas</h1>
+          <p className="ideas__explanation">
+            You can create your own movie ideas and see them as cards below the creation form
+          </p>
+          <MovieForm addNewItemCb={(movie: IMovie) => setMovies([...movies, movie])} />
+          {movies.length > 0 && <MovieCardsList movies={movies} />}
+        </div>
+      </main>
+    </div>
+  );
 }
--- react-movie-app/src/ui/CheckboxItem/CheckboxItem.tsx
+++ react-movie-app/src/ui/CheckboxItem/CheckboxItem.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
 import './CheckboxItem.css';
 
 interface ICheckBoxItemProps {
   name: string;
   labelText?: string;
+  register?: UseFormRegisterReturn;
 }
 
 export default function CheckboxItem({
   name,
   labelText = name[0].toUpperCase() + name.slice(1),
+  register,
 }: ICheckBoxItemProps) {
   return (
     <div className="checkbox-item">
-      <input id={name} type="checkbox" className="checkbox-item__checkbox" name={name} />
+      <input
+        {...register}
+        id={name}
+        type="checkbox"
+        className="checkbox-item__checkbox"
+        value={name}
+      />
       <label htmlFor={name} className="checkbox-item__checkmark"></label>
       <label htmlFor={name} className="checkbox-item__label">
         {labelText}
